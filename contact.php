32a6cf252cf89fc4ec2873a4a040d7d973f0d6fe
Monika Mateska <monika.mateska@netcetera.com>
2022-11-22 22:22:38 +0100

    updated page title

--- contact.php
+++ contact.php
@@ -25,7 +25,7 @@ $email_subject = "New Form submission";
 $email_body = "You have received a new message from the user $name.\n".
     "Here is the message:\n $message".
     
-$to = "monika.mateska@cocoaheads.mk";//<== update the email address
+$to = "contact@cocoaheads.mk";//<== update the email address
 $headers = "From: $visitor_email \r\n";
 $headers .= "Reply-To: $visitor_email \r\n";
 //Send the email!
